7b72220895275c5392c76a7b1b2030cabfdb368d
Squad024-2023 <fotografiasummer@gmail.com>
2024-01-21 15:52:26 -0300

    Sessão 2 primeiro passo feito

--- app/o-que-fazemos/page.tsx
+++ app/o-que-fazemos/page.tsx
@@ -2,6 +2,7 @@ import styles from './o-que-fazemos.module.css'
 import Image from 'next/image'
 import student from '@/public/images/studentNotebook.webp'
 import students from '@/public/images/studentsNotebook.webp'
+import problems from '@/public/images/problems.webp'
 
 export default function OqueFazemos() {
 
@@ -37,11 +38,32 @@ export default function OqueFazemos() {
       <section className={styles.section2}>
         <div className={styles.tela2}>
           <div className={styles.textoTela2}>
-          <h1>Como Nós Fazemos?</h1>
-          <h5>Nos desenvolvemos monitorias e mentorias com um método de ensino voltado para facilitar a aprendizagem e a comunicação.
-            Visamos transmitir conhecimento de forma simples e objetiva, para que o aluno possa aprender de forma mais rápida e eficiente.
-          </h5>
+            <h1>Como Nós Fazemos?</h1>
+            <h5>Nos desenvolvemos monitorias e mentorias com um método de ensino voltado para facilitar a aprendizagem e a comunicação.
+              Visamos transmitir conhecimento de forma simples e objetiva, para que o aluno possa aprender de forma mais rápida e eficiente.
+            </h5>
           </div>
+
+
+          <div className={styles.comoContainer}>
+            <div className={styles.textoComo}>
+              <h2>Conhecendo os problemas</h2>
+              <h5>Várias dificuldades podem ser encontradas na vida acadêmica. Conhecer os problemas atuais dos estudantes é fundamental para desenvolver
+                planos de estudo e orientação da forma mais eficiente possível.
+              </h5>
+            </div>
+            <Image
+              className={styles.imagemTextoComo1}
+              src={problems}
+              priority
+              quality={100}
+              placeholder='blur'
+              alt='Imagem de estudante com notebook'
+            />
+          </div>
+
+
+
         </div>
       </section>
     </main>
