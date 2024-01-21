e151176a81f4df075928e60f8f7bfa33b0427a72
Squad024-2023 <fotografiasummer@gmail.com>
2024-01-20 23:29:27 -0300

    Feature: sessão 1 concluída. sessão 2 a caminho

--- app/quem-somos/page.tsx
+++ app/quem-somos/page.tsx
@@ -13,7 +13,7 @@ export default function QuemSomos() {
             <section className={styles.section1}>
                 <div className={styles.tela1}>
                     <div className={styles.textoTela1}>
-                        <h1>QUEM SOMOS?</h1>
+                        <h1>Quem Somos?</h1>
                         <h2> monitorando esta aqui para ajudar estudantes a seguirem seus sonhos, oferecendo monitorias e mentorias em disciplinas acadêmicas e sociais.</h2>
                         <p>
                             Nosso foco é principalmente nos estudantes que não tem uma boa educação base do ensino médio e se deparam com dificuldades imensas
@@ -24,17 +24,73 @@ export default function QuemSomos() {
                 </div>
             </section>
             <section className={styles.section2}>
-                <div className={styles.tela2}>
-                    <div className={styles.textoTela2}>
-                        <h1>QUEM SOMOS?</h1>
-                        <h2> monitorando ta aqui para ajudar estudantes a seguirem seus sonhos, oferecendo monitorias e mentorias em disciplinas acadêmicas e sociais.</h2>
-                        <p>
-                            Nosso foco é principalmente nos estudantes que não tem uma boa educação base do ensino médio e se deparam com dificuldades imensas
-                            logo nos seus primeiros meses de faculdade.
-                        </p>
-
-                    </div>
+                <div className={styles.descricaoTela2}>
+                    <h1>Nosso Time</h1>
+                    <p>Nosso time sentiu todas as dificuldades que um aluno de faculdade pode passar. Juntos nessa ideia
+                        de ajudar os outros, criamos o monitorando para que todos possam ter uma oportunidade de seguir seus sonhos.
+                    </p>
                 </div>
+                <div className={styles.cardsTela2}>
+                    <div className={styles.cardContainer}>
+                        <div className={styles.cardFoto}>
+                        </div>
+                        <div className={styles.cardTexto}>
+                            <h1>Arthur Bernard</h1>
+                            <p>VAMO DALE PRA NÃO TOMALE CARAI
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                Nobis dolores, fugit laudantium, distinctio eum itaque laboriosam
+                                nemo sequi cumque culpa doloribus vitae. Accusantium vero doloremque
+                                provident hic saepe itaque quibusdam.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.cardContainer}>
+                        <div className={styles.cardFoto}>
+                        </div>
+                        <div className={styles.cardTexto}>
+                            <h1>Nayara Pereira</h1>
+                            <p>Função Função Função Função Função
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                Nobis dolores, fugit laudantium, distinctio eum itaque laboriosam
+                                nemo sequi cumque culpa doloribus vitae. Accusantium vero doloremque
+                                provident hic saepe itaque quibusdam.
+
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className={styles.cardContainer}>
+                        <div className={styles.cardFoto}>
+                        </div>
+                        <div className={styles.cardTexto}>
+                            <h1>Nome</h1>
+                            <p>Função</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.cardContainer}>
+                        <div className={styles.cardFoto}>
+                        </div>
+                        <div className={styles.cardTexto}>
+                            <h1>Nome</h1>
+                            <p>Função</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.cardContainer}>
+                        <div className={styles.cardFoto}>
+                        </div>
+                        <div className={styles.cardTexto}>
+                            <h1>Nome</h1>
+                            <p>Função</p>
+                        </div>
+                    </div>
+
+
+                </div>
+
+
             </section>
         </main>
     )
