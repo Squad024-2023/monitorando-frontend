0d8e00898519ca9f6e8ca155a1c967b82634c39a
Squad024-2023 <fotografiasummer@gmail.com>
2024-01-21 15:17:57 -0300

    Feature: sessão 2 base

--- app/o-que-fazemos/page.tsx
+++ app/o-que-fazemos/page.tsx
@@ -9,26 +9,19 @@ export default function OqueFazemos() {
   return (
     <main className={styles.main}>
       <section className={styles.section1}>
-
         <div className={styles.tela1}>
           <div className={styles.textoTela1}>
             <h1>O Que Fazemos?</h1>
-
-            {/* <h5>Nos desenvolvemos monitorias e mentorias com um método de ensino voltado para facilitar a aprendizagem e a comunicação.
-              Visamos transmitir conhecimento de forma simples e objetiva, para que o aluno possa aprender de forma mais rápida e eficiente.
-            </h5> */}
             <h5>Atualmente temos mais de 200 mil horas de aprendizado, divididas entre mentorias e monitorias para alunos em mais de 15 estados brasileiros e 300 universidades diferentes.</h5>
-            
-          <Image
-            className={styles.imagemTextoTela1}
-            src={students}
-            priority
-            quality={100}
-            placeholder='blur'
-            alt='Imagem de estudante com notebook'
-          />
+            <Image
+              className={styles.imagemTextoTela1}
+              src={students}
+              priority
+              quality={100}
+              placeholder='blur'
+              alt='Imagem de estudante com notebook'
+            />
           </div>
-
           <Image
             className={styles.imagemTela1}
             src={student}
@@ -38,13 +31,17 @@ export default function OqueFazemos() {
             alt='Imagem de estudante com notebook'
           />
         </div>
-
       </section>
+
+
       <section className={styles.section2}>
-
-
         <div className={styles.tela2}>
-          <div className={styles.conteudo}></div>
+          <div className={styles.textoTela2}>
+          <h1>Como Nós Fazemos?</h1>
+          <h5>Nos desenvolvemos monitorias e mentorias com um método de ensino voltado para facilitar a aprendizagem e a comunicação.
+            Visamos transmitir conhecimento de forma simples e objetiva, para que o aluno possa aprender de forma mais rápida e eficiente.
+          </h5>
+          </div>
         </div>
       </section>
     </main>
