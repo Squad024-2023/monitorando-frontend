237f825a21cd2d75de577118203d3e9d0084413b
Squad024-2023 <fotografiasummer@gmail.com>
2024-01-21 17:09:16 -0300

    Feature: sessão 2 concluída

--- app/o-que-fazemos/page.tsx
+++ app/o-que-fazemos/page.tsx
@@ -47,9 +47,8 @@ export default function OqueFazemos() {
 
           <div className={styles.comoContainer}>
             <div className={styles.textoComo}>
-              <h2>Conhecendo os problemas</h2>
-              <h5>Várias dificuldades podem ser encontradas na vida acadêmica. Conhecer os problemas atuais dos estudantes é fundamental para desenvolver
-                planos de estudo e orientação da forma mais eficiente possível.
+              <h2>Conhecendo os Problemas</h2>
+              <h5>Várias dificuldades podem ser encontradas na vida acadêmica. Através de pesquisas e experiências com instituições de ensino, conseguimos identificar os principais problemas que os alunos enfrentam em seus campos.
               </h5>
             </div>
             <Image
@@ -60,6 +59,46 @@ export default function OqueFazemos() {
               placeholder='blur'
               alt='Imagem de estudante com notebook'
             />
+          </div>
+
+
+          <div className={styles.comoContainer}>
+            <Image
+              className={styles.imagemTextoComo2}
+              src={problems}
+              priority
+              quality={100}
+              placeholder='blur'
+              alt='Imagem de estudante com notebook'
+            />
+            <div className={styles.textoComo}>
+              <h2>Trabalhando na Solução</h2>
+              <h5>Nosso monitores e mentores são treinados para desenvolver planos de estudos e orientações de forma eficiente. Focados nas resoluções dos problemas, eles são capazes de ajudar os alunos a superarem suas dificuldades.
+              </h5>
+            </div>
+          </div>
+
+
+          <div className={styles.comoContainer}>
+            <div className={styles.textoComo}>
+              <h2>Verificando Resultados</h2>
+              <h5>Após o período de 1 semestre de aplicações de aulas e orientações, retornamos o contato a instuição qual o aluno pertence para verificar os resultados obtidos. Com isso, conseguimos identificar se o aluno conseguiu superar suas dificuldades e se o nosso trabalho foi eficiente.
+              </h5>
+            </div>
+            <Image
+              className={styles.imagemTextoComo1}
+              src={problems}
+              priority
+              quality={100}
+              placeholder='blur'
+              alt='Imagem de estudante com notebook'
+            />
+          </div>
+
+          <div className={styles.textoTela2}>
+            <h1>Repetir e Melhorar</h1>
+            <h5>A medida que mais alunos e mais ciclos se iniciam, repetimos o processo de identificação de problemas e soluções, sempre buscando melhorar a qualidade do nosso trabalho e a eficiência das nossas aulas e orientações.
+            </h5>
           </div>
 
 
