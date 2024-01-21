8e5657ca14b46cf0a14f50626c1b74395e7fd8a8
Squad024-2023 <fotografiasummer@gmail.com>
2024-01-21 03:42:46 -0300

    Feature: interação do botão da navbar com a página quem somos

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,27 +1,57 @@
 'use client'
-import { useState } from "react";
-import { usePathname } from "next/navigation";
-import styles from "./nav.module.css";
-import Link from "next/link";
-import Logo from "@/components/logo/logo";
-import Doar from "../doar/doar";
+import { useState, useRef, useEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import styles from './nav.module.css';
+import Link from 'next/link';
+import Logo from '@/components/logo/logo';
+import Doar from '../doar/doar';
+import hamb from '@/public/icons/hamburguer.json'
+import lottie from 'lottie-web';
 
 export default function Nav() {
   const pathname = usePathname();
   const [drop, setDrop] = useState<boolean>(true);
-
   const handleClickEvent = () => {
+    if (aniInstanceRef.current) {
+      if (drop) {
+        aniInstanceRef.current.playSegments([0, 60], true);
+      } else {
+        aniInstanceRef.current.playSegments([60, 0], true);
+      }
+    }
+
     setDrop(!drop);
   };
 
 
+  const container = useRef<HTMLDivElement | null>(null) //necessário para a animação do lottie funcionar no hover do botão inteiro
+  const aniInstanceRef = useRef<any>(null) //necessário para cada botão ter sua própria instância do lottie 
+
+  useEffect(() => {
+    if (container.current) {
+      aniInstanceRef.current = lottie.loadAnimation({
+        container: container.current,
+        loop: false,
+        autoplay: false,
+        animationData: hamb,
+
+      });
+
+      return () => {
+        if (aniInstanceRef.current) {
+          aniInstanceRef.current.destroy();
+        }
+      };
+    }
+  }, [hamb]);
+
+ 
+
   return (
     <nav className={styles.nav}>
       <Logo />
-      <button className={styles.btn}
-        onClick={handleClickEvent}
-      >
-        xzinho
+      <button className={styles.btn} onClick={handleClickEvent}>
+        <div ref={container} className={styles.lottie}></div>
       </button>
       <menu className={`${styles.menuNav} ${drop ? styles.hide : ''}`}>
         <li><Link className={pathname === '/quem-somos' ? styles.active : ''} href='/quem-somos'>Quem Somos</Link></li>
